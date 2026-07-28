2aaca89f48cf80c2b6edf399340d861e2e43c0c6
PHILIP <terhemeniorwua@gmail.com>
2026-07-28 17:27:33 +0100

    created the admin dashboard

--- script.js
+++ script.js
@@ -125,4 +125,27 @@ fetchProducts().then(res =>{
         </div>
         `
    })
-})
+});
+
+
+  const sidebar = document.getElementById('sidebar');
+  const overlay = document.getElementById('sidebarOverlay');
+  const openBtn = document.getElementById('openSidebar');
+  const closeBtn = document.getElementById('closeSidebar');
+
+  function openSidebar() {
+    sidebar.classList.remove('-translate-x-full');
+    overlay.classList.remove('hidden');
+  }
+  function closeSidebar() {
+    sidebar.classList.add('-translate-x-full');
+    overlay.classList.add('hidden');
+  }
+
+ if(openBtn){
+     openBtn.addEventListener('click', openSidebar);
+ }
+  if(closeBtn){
+    closeBtn.addEventListener('click', closeSidebar);
+  }
+  if(overlay){overlay.addEventListener('click', closeSidebar);}
