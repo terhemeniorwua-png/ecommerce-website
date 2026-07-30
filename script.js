ffdf86ba248469c694b617a3ab827524d76afded
PHILIP <terhemeniorwua@gmail.com>
2026-07-30 23:10:26 +0100

    started the search method

--- script.js
+++ script.js
@@ -92,6 +92,8 @@ let dialogMssg = document.querySelector('.erromssg');
 let cancelBtn = document.querySelector('.cancelBtn');
 let categories = document.querySelector('.nav');
 
+
+
 // saving categories in the dashbord
 
 const fetchProducts = async ()=>{
@@ -116,11 +118,12 @@ return reponse
 }
 
 
-
+//adding nav bar
 
 let products = document.querySelector('.products')
 
 const uniqueProduct = new Set()
+
 
 fetchProducts().then(res =>{
 
@@ -135,15 +138,17 @@ fetchProducts().then(res =>{
    })
 
 
+    let html = ""
 
   res.products.forEach(product=>{
-    if(products)
-    products.innerHTML += `  <div>
+   
+    // if(products)
+    products.innerHTML  += `  <div>
             <div class="relative shadow-2xl">
                 <img src="${product.thumbnail}" alt=""><span class="absolute top-0 left-3 text-2xl"><i class="fa-regular fa-heart"></i></span>
             </div>
             <div class="pt-5 px-5 space-y-5">
-                <h3 class="font-bold">${product.title}</h3>
+                <h3 class="title font-bold">${product.title}</h3>
                 <div class="flex items-center gap-2">
                 <i class="fa-regular fa-star text-amber-500"></i>
                 <i class="fa-regular fa-star text-amber-500"></i>
@@ -158,55 +163,95 @@ fetchProducts().then(res =>{
             </div>
         </div>
         `
+        // console.log(products)
    })
-
+// products.innerHTML = html
 });
 
 let searchBtn = document.querySelector('.searchBtn');
 let SearchInput = document.querySelector('.SearchInput');
+// let productTitle = 
 
-const searchFetched = async ()=>{
 
-    let value = SearchInput.value.trim()
-    if(value === '')return;
-
-    try{
-        let fetchRequest = await fetch(`https://dummyjson.com/products/search?=${value}`);
-        let result = await fetchRequest.json()
-      displayProducts(result.product)
-
-    } catch(e){
-    dialogHeading.innerText = 'Error';
-    dialogMssg.innerText = e.message;
-    dialogbox.showModal()
+SearchInput.addEventListener('keyup', e =>{
+    const inputValue = e.target.value.toLowerCase();
+    let productNames = document.querySelectorAll('h3 .title')
+    // productNames.forEach(product =>{
+    //     if(product.toLowerCase().includes(inputValue)){
+    //         console.log('Right')
+    //     }
+    // })
+    console.log(productNames.textContent.toLowerCase())
+})
     
-     cancelBtn.addEventListener('click', ()=>{
 
-    dialogbox.close()
-    })
-}
+
+
+
+
+//     const inputValue = SearchInput.value.toLowerCase;
+
+//     Array.from(productTitle).forEach(product=>{
+
+//          if(inputValue !== product[productTitle].toLowerCase){
+//         console.log('Nothing match')
+//     } else{
+//         console.log('product')
+//     }
+//     })
+
+//  }
+// // searchBtn.addEventListener('click', searchFetched)
+
+
+
+
+//     let value = SearchInput.value.trim()
+//     if(value === '')return;
+
+//     try{
+//         let fetchRequest = await fetch(`https://dummyjson.com/products/search?=${value}`);
+//         let result = await fetchRequest.json()
+//       displayProducts(result.product)
+
+//     } catch(e){
+//     dialogHeading.innerText = 'Error';
+//     dialogMssg.innerText = e.message;
+//     dialogbox.showModal()
     
-}
+//      cancelBtn.addEventListener('click', ()=>{
+
+//     dialogbox.close()
+//     })
+// }
+    
+// }
 
 
-function displayProducts(prod) {
-    products.innerHTML = "";
+// function displayProducts(prod) {
+//     products.innerHTML = "";
 
-    if (prod.length === 0) {
-        productsContainer.innerHTML = "<h2>No product found.</h2>";
-        return;
-    }
+//     if (prod.length === 0) {
+//         productsContainer.innerHTML = "<h2>No product found.</h2>";
+//         return;
+//     }
 
-    prod.forEach(product => {
-        products.innerHTML += `
-            <div class="card">
-                <img src="${product.thumbnail}" width="150">
-                <h3>${product.title}</h3>
-                <p>$${product.price}</p>
-            </div>
-        `;
-    });
-}
+//     prod.forEach(product => {
+//         products.innerHTML += `
+//             <div class="card">
+//                 <img src="${product.thumbnail}" width="150">
+//                 <h3>${product.title}</h3>
+//                 <p>$${product.price}</p>
+//             </div>
+//         `;
+//     });
+// }
+
+
+
+
+
+
 
 
 //  searchBtn.addEventListener('click', searchFetched)
